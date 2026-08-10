3ec6a852775a8709e3e6f9241a6a378546a7aaf1
harnug14 <harnug14@gmail.com>
2026-08-11 00:05:54 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,14 @@
+import { NextResponse, type NextRequest } from "next/server";
 
+export async function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * MURNI MEM-BYPASS /auth/callback DAN /login AGAR VERCEL TIDAK MENCEGATNYA
+     */
+    "/((?!_next/static|_next/image|favicon.ico|auth/callback|login|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
