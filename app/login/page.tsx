8a6c28b220aa9d76b5ac475286e8bc3820d604fe
harnug14 <harnug14@gmail.com>
2026-08-10 23:45:04 +0700

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,7 +15,7 @@ export default function LoginPage() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: "https://harnug-studio-full.vercel.app/",
+          redirectTo: `${window.location.origin}/auth/callback`,
         },
       });
 
